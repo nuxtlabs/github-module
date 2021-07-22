902f9d8e08b535292d9ea3eb4f26d44cd72b0ae7
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-07-22 14:52:07 +0200

    :bug: (tests) switch back to basic jest; fix tests (temp)

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,22 @@
-const { jsWithBabelESM: tsjPreset } = require('ts-jest/presets')
-
 module.exports = {
-  preset: 'ts-jest',
   transform: {
-    ...tsjPreset.transform
+    '\\.(js|ts)$': [
+      'babel-jest',
+      {
+        presets: ['@babel/preset-env', '@babel/preset-typescript'],
+        plugins: [
+          '@babel/plugin-transform-runtime',
+          '@babel/plugin-proposal-class-properties',
+          '@babel/plugin-proposal-object-rest-spread'
+        ]
+      }
+    ]
   },
   // TODO: Fix coverage
   collectCoverage: false,
-  collectCoverageFrom: ['src/**', '!templates/**', '!example/**', '!.nuxt/**'],
-  coveragePathIgnorePatterns: ['node_modules', '.nuxt']
+  collectCoverageFrom: [],
+  coveragePathIgnorePatterns: ['node_modules', '.nuxt'],
+  transformIgnorePatterns: [
+    'node_modules/(?!@docus/app/|nuxt-i18n|mdast-util-to-hast|unist-builder|detab|unist-util-position/*)'
+  ]
 }
