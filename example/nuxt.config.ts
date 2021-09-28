6d20067fac2d0b6b9eb80ccd17caf283ffee9d72
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-09-28 14:35:19 +0200

    :sparkles: (example) update example

--- example/nuxt.config.ts
+++ example/nuxt.config.ts
@@ -1,8 +1,17 @@
+import { resolve } from 'path'
 import { withDocus } from '@docus/app/kit'
+
+const modulePath = resolve(__dirname, '../src/index')
 
 const config = withDocus({
   rootDir: __dirname,
-  buildModules: ['../src']
+  buildModules: [modulePath],
+  nitro: {
+    externals: {
+      inline: [],
+      external: ['remark-github']
+    }
+  }
 })
 
 export default config
