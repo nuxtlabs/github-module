7b812cec6f7fe7bdce967242eeecb788f3cdd869
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-09-28 14:35:34 +0200

    :sparkles: (example) ..

---
+++ example/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div>Docus default app</div>
+    <div v-for="release in releases" :key="release.name">
+      <ProseH1>{{ release.name }}</ProseH1>
+      <DocusContent :document="release" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import { useContent } from '@docus/app'
+
+export default defineComponent({
+  async asyncData() {
+    const content = useContent()
+
+    const { releases } = await content.fetch('github-releases')
+
+    return {
+      releases
+    }
+  }
+})
+</script>
