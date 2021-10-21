170dc8e8097ee4bdf3281d88894c6a80f59d81f0
Farnabaz <farnabaz@gmail.com>
2021-10-21 15:42:33 +0330

    fix: disable cache

--- src/runtime/server/api/releases.ts
+++ src/runtime/server/api/releases.ts
@@ -1,16 +1,17 @@
-import { cachify } from '../utils/cache'
+// import { cachify } from '../utils/cache'
 import { fetch } from '../utils/github'
 // @ts-ignore
 import config from '#config'
 
-const isProduction = process.env.NODE_ENV === 'production'
+// const isProduction = process.env.NODE_ENV === 'production'
 
-const withCache = (name: string, fn: any) => (isProduction ? cachify(fn, { name, swr: true, ttl: 60000 }) : fn)
+// const withCache = (name: string, fn: any) => (isProduction ? cachify(fn, { name, swr: true, ttl: 60000 }) : fn)
 
-const fetchReleases = withCache('github-releases', () => fetch(config.github))
+// const fetchReleases = withCache('github-releases', () => fetch(config.github))
 
 export default async function githubReleases() {
-  const releases = await fetchReleases()
+  // const releases = await fetchReleases()
+  const releases = await fetch(config.github)
 
   return {
     releases
