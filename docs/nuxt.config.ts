077bda1a7f0e0f9d84e25670e9a480ad1ae8bd89
Yaël Guilloux <yael.guilloux@gmail.com>
2022-07-27 01:42:28 +0200

    chore(cleanup): up deps ; cleanup

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,6 +1,8 @@
 import { defineNuxtConfig } from 'nuxt'
 
 export default defineNuxtConfig({
+  app: {
+  },
   extends: [
     '../node_modules/@nuxt-themes/docus'
   ],
@@ -16,28 +18,5 @@ export default defineNuxtConfig({
       prefix: '',
       global: true
     }
-  ],
-  tailwindcss: {
-    config: {
-      theme: {
-        extend: {
-          colors: {
-            primary: {
-              DEFAULT: '#4183C4',
-              50: '#CDDEF0',
-              100: '#BED4EB',
-              200: '#9EC0E1',
-              300: '#7FACD7',
-              400: '#6097CE',
-              500: '#4183C4',
-              600: '#31679C',
-              700: '#234B72',
-              800: '#162F47',
-              900: '#09121C'
-            }
-          }
-        }
-      }
-    }
-  }
+  ]
 })
