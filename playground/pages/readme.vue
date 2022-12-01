91266e0dee5c671672e64c6b3d4bbc676428d161
Levi (Nguyễn Lương Huy) <huy.nguyen.luong96@gmail.com>
2022-12-02 00:15:15 +0700

    fix: unable to create buffer from empty readme (#39)
    * fix: unable to create buffer from empty readme
    
    * fix: narrow `remarkPlugins` type

--- playground/pages/readme.vue
+++ playground/pages/readme.vue
@@ -6,13 +6,19 @@ const qRepo = ref('supabase-module')
 <template>
   <div>
     <GithubReadme v-slot="{ readme, refresh }" :query="{ owner: qOwner, repo: qRepo }">
-      Fetch readme from query:
+      Fetch README from query:
       <input v-model="qOwner" style="margin-left: 1rem;" type="text"> /
       <input v-model="qRepo" type="text">
       <button style="margin-left: 1rem;" @click="refresh">
         Search
       </button>
-      <ContentRenderer :value="readme" />
+      <ContentRenderer :value="readme">
+        <template #empty>
+          <div style="margin: 1rem 0">
+            README not found
+          </div>
+        </template>
+      </ContentRenderer>
     </GithubReadme>
   </div>
 </template>
